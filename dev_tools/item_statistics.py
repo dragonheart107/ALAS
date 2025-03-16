548234ad159646de4662c7012409d0a24945f94f
dragonheart107 <drachen2000@t-online.de>
2025-03-16 19:29:21 +0100

    test

--- dev_tools/item_statistics.py
+++ dev_tools/item_statistics.py
@@ -5,8 +5,9 @@ from tqdm import tqdm
 
 import module.config.server as server
 
-server.server = 'cn'  # Edit your server here.
+server.server = 'en'  # Edit your server here.
 
+from module.base.utils import load_image
 from module.logger import logger
 from module.statistics.battle_status import BattleStatusStatistics
 from module.statistics.get_items import GetItemsStatistics
