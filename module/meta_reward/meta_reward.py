362ea986260a36d0cc3e044d1163026e5093916e
dragonheart107 <drachen2000@t-online.de>
2025-04-10 23:55:45 +0200

    groundwork for meta enhance;untested

--- module/meta_reward/meta_reward.py
+++ module/meta_reward/meta_reward.py
@@ -1,3 +1,5 @@
+import module.config.server as server
+
 from module.base.timer import Timer
 from module.combat.combat import Combat
 from module.logger import logger
@@ -7,6 +9,14 @@ from module.ui.assets import BACK_ARROW
 from module.ui.page import page_meta_menu, page_meta_beacon, page_meta_dossier, page_meta_lab, page_meta_dos_reward, page_reward
 from module.ui.ui import UI
 
+from module.base.button import ButtonGrid
+from module.base.decorator import cached_property
+from module.ui.setting import Setting
+from module.ui.scroll import Scroll
+
+
+def _server_support_meta_upgrade():
+    return server.server in ['en']
 
 class BeaconReward(Combat, UI):
     def meta_reward_notice_appear(self):
@@ -278,3 +288,149 @@ class MetaReward(BeaconReward, DossierReward):
             DossierReward(self.config, self.device).run()
         else:
             logger.info(f'Possible wrong parameter {category}, please contact the developers.')
+
+LAB_SCROLL = Scroll(LAB_SCROLL, name='LAB_SCROLL')
+
+SHIP_GRIDS_SELECT = ButtonGrid(
+    origin=(8, 405), delta=(0, 75), button_shape=(226, 69), grid_shape=(1, 1), name='META_LAB_SHIPS') #select first landing page 1st
+
+SHIP_GRIDS = ButtonGrid(
+    origin=(8, 480), delta=(0, 47), button_shape=(133, 41), grid_shape=(1, 4), name='META_LAB_SHIPS') #non select landing page 2nd
+
+class MetaLabFilter(Button, UI):
+    def handle_dock_cards_loading(self):
+        # Poor implementation.
+        self.device.sleep((1, 1.5))
+        self.device.screenshot()    
+
+    def dock_filter_enter(self):
+        self.ui_click(LAB_FILTER, appear_button=LAB_LIST_CHECK, check_button=LAB_FILTER_CONFIRM,
+                      skip_first_screenshot=True)
+
+    def dock_filter_confirm(self, wait_loading=True):
+        """
+        Args:
+            wait_loading: Default to True, use False on continuous operation
+        """
+        self.ui_click(LAB_FILTER_CONFIRM, check_button=LAB_LIST_CHECK, skip_first_screenshot=True)
+        if wait_loading:
+            self.handle_dock_cards_loading()
+
+    @cached_property
+    def dock_filter(self) -> Setting:
+        delta = (147 + 1 / 3, 57)
+        button_shape = (139, 42)
+        setting = Setting(name='METALAB', main=self)
+        setting.add_setting(
+            setting='type',
+            option_buttons=ButtonGrid(
+                origin=(219, 213), delta=delta, button_shape=button_shape, grid_shape=(7, 2), name='FILTER_TYPE'),
+            option_names=['all', 'vanguard', 'main', 'dd', 'cl', 'ca', 'bb', 'cv', 'ar', 'ss', 'others', 'not_available', 'not_available', 'not_available'],
+            option_default='all'
+        )
+        setting.add_setting(
+            setting='rarity',
+            option_buttons=ButtonGrid(
+                origin=(219, 343), delta=delta, button_shape=button_shape, grid_shape=(7, 1), name='FILTER_RARITY'),
+            option_names=['all', 'elite', 'super_rare', 'not_available', 'not_available', 'not_available', 'not_available'],
+            option_default='all'
+        )
+        setting.add_setting(
+            setting='extra',
+            option_buttons=ButtonGrid(
+                origin=(219, 416), delta=delta, button_shape=button_shape, grid_shape=(7, 1), name='FILTER_EXTRA'),
+            option_names=['no_limit', 'enhanceable', 'can_level_skill', 'can_limit_break', 'not_available', 'not_available', 'not_available'],
+            option_default='no_limit'
+        )
+        return setting
+
+    def dock_filter_set(
+            self,
+            type='all',
+            rarity='all',
+            extra='no_limit',
+            wait_loading=True
+    ):
+        """
+        A faster filter set function.
+
+        Args:
+            type (str, list):
+                ['all', 'vanguard', 'main', 'dd', 'cl', 'ca', 'bb', 'cv', 'ar', 'ss', 'others', 'not_available', 'not_available', 'not_available']
+            rarity (str, list):
+                ['all', 'elite', 'super_rare', 'not_available', 'not_available', 'not_available''not_available']
+            extra (str, list):
+                ['no_limit', 'enhanceable', 'can_level_skill', 'can_limit_break', 'not_available', 'not_available', 'not_available']
+
+        Pages:
+            in: page_dock
+        """
+        self.dock_filter_enter()
+        self.dock_filter.set(type=type, rarity=rarity, extra=extra)
+        self.dock_filter_confirm(wait_loading=wait_loading)
+
+class MetaEnhance(UI, MetaLabFilter):
+    def meta_enhance(self, skip_first_screenshot=True):
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if self.appear_then_click(META_MAT):
+                continue
+            if self.appear(META_NOMAT):
+                break
+
+    def meta_enhance_select(self, skip_first_screenshot=True):
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+            self.wait_until_stable(META_REL)
+            if not self.appear(META_FP_LOCK) and self.appear_then_click(META_FP):
+                self.meta_enhance()
+                continue
+            if not self.appear(META_TORP_LOCK) and self.appear_then_click(META_TORP):
+                self.meta_enhance()
+                continue
+            if not self.appear(META_AVI_LOCK) and self.appear_then_click(META_AVI):
+                self.meta_enhance()
+                continue
+            if self.appear_then_click(META_REL):
+                self.meta_enhance()
+                break
+
+    
+    def meta_limit_enhance():
+        'do sth'
+        
+
+    def meta_skill_select():
+        'do sth'
+
+    def run(self):
+        if self.config.SERVER in ['en']:
+            pass
+        else:
+            logger.info(f'MetaReward is not supported in {self.config.SERVER}, please contact server maintainers')
+            return
+        self.ui_ensure(page_meta_lab)
+            #self.config.OpsiAshBeacon_
+        if not self.appear(LAB_LIST_CHECK):
+            self.device.click(BACK_ARROW)
+        while 1:
+            self.dock_filter_set(extra='enhanceable')
+            if self.appear_then_click(META_ENHANCE):
+                while 1:
+                    self.meta_enhance_select()
+            self.dock_filter_set(extra='can_level_skill')
+            if self.appear_then_click(META_SKILL):
+                while 1:
+                    self.meta_skill_select()
+            self.dock_filter_set(extra='can_limit_break')
+            if self.appear_then_click(META_LIMIT):
+                while 1:
+                    self.meta_limit_enhance()
+#meta rel appears last wait fpor stabel
